d7499d2e96cebaf8d9f6b991f958f3ed1485459b
Hojiakbar Ruziyev <98095043+HojiakbarR-dev@users.noreply.github.com>
2025-11-04 06:31:35 -0500

    Create task 1.py

---
+++ lesson-1/homework/task 1.py
@@ -0,0 +1,33 @@
+
+# Task 1
+side = float(input("Enter side of square: "))
+Perimeter = 4*side
+area = side * side
+print("The Perimeter is: ", Perimeter)
+print("The Area is: ", area)
+
+
+
+# Task 2
+import math
+diameter = float(input("Enter the diameter of the circle: "))
+Len_circle = math.pi * diameter
+print("The length of the circle: ", Len_circle)
+
+
+# Task 3 
+a = float(input("Enter first number a: "))
+b = float(input("Enter second number b: "))
+mean = (a + b) / 2
+print("Mean of a and b =", mean)
+
+# Task 4 
+sum_ab = a + b
+product_ab = a * b
+square_a = a ** 2
+square_b = b ** 2
+
+print("Sum =", sum_ab)
+print("Product =", product_ab)
+print("Square of a =", square_a)
+print("Square of b =", square_b)
